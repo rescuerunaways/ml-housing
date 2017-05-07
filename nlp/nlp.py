adec9d40f5c54a573581ae280cc6f60036c394e7
Your Name <ermolaeva@agent.ru>
2017-05-07 15:06:29 +1000

    dictionary indexes - all words, no duplicates

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -1,3 +1,6 @@
+import operator
+from functools import reduce
+
 import pandas as pd
 
 
@@ -6,8 +9,13 @@ def clean_data():
     df = df.apply(lambda x: x.astype(str).str.lower()) \
         .apply(lambda x: x.astype(str).str.split('[^a-z]')) \
         .applymap(lambda x: list(filter(None, x)))
+    return df
 
-    print(df)
+
+def words(df):
+    df = df.apply(lambda x: reduce(operator.add, x)) \
+        .apply(lambda x: list(set(x)))
+    return dict(enumerate(df))
 
 
-clean_data()
+print(words(clean_data()))
